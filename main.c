cc020c173d6af08b561c2a418fbbdbd611f9bef1
FelipeABruschi <felipeaugbr@outlook.com>
2024-04-09 18:36:43 -0300

    comecando menu

--- main.c
+++ main.c
@@ -4,13 +4,41 @@
 
 int main(void)
 {
-    char pessoa[] = "felipe";
+    Lista_contatos* lista = criar_lista_ctt();
+    ctt pessoa;
 
-    int tamanho = strlen(pessoa);
+    int opcao;
 
-    for(int i = 0; i < tamanho; i++)
-        printf("%c", pessoa[i]);
-    printf("\n");
+    do
+    {
+        printf("--------LISTA--DE--CONTATOS---------\n");
+        printf("ESCOLHA UMA OPCAO:\n");
+        printf("(1) adicionar contato\n");
+        printf("(2) imprimir contatos\n");
+        printf("(3) buscar contatos\n");
+        printf("(4) excluir contato\n");
+        printf("(5) editar contatos\n");
+        printf("(0) sair da aplicacao.\n");
+
+        scanf("%d: ", &opcao);
+
+        switch (opcao)
+        {
+            case 1:
+                printf("digite o nome, telefone e email do contato:\n");
+                char nome[50];
+                fgets(nome, 50, stdin);
+                int numero = scanf("%d", &numero);
+                char mail[10];
+                scanf("%s\n", mail);
+                strcpy(pessoa.nome, nome);
+                pessoa.numero = numero;
+                strcpy(pessoa.email, mail);
+                add_contato(lista, pessoa);
+        }
+
+
+    }while(opcao != 0);
 
     return 0;
 }
