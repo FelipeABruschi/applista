d412de9e74863984457ee3c6a9a887d95499f703
FelipeABruschi <110953697+FelipeABruschi@users.noreply.github.com>
2024-04-22 09:30:21 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <libprg/libprg.h>
 
+void mostrar(Lista_encadeada *lista)
+{
+    no *aux = lista->inicio;
+
+    while(aux)
+    {
+        printf("%d ", aux->valor);
+        aux = aux->proximo;
+    }
+    printf("\n");
+}
+
 int main()
 {
-    
+    Lista_encadeada mylist;
+
+    criar_lista_encadeada(&mylist, 0);
+
+    inserir_no(&mylist, 5);
+    inserir_no(&mylist, 10);
+    inserir_no(&mylist, 11);
+    inserir_no(&mylist, 8);
+
+    mostrar(&mylist);
 
     return 0;
 }
