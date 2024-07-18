76cafe2f9bf86bbaebcfdbe1c427386b875efa09
FelipeABruschi <felipeaugbr@outlook.com>
2024-07-17 22:50:56 -0300

    testando ordenacao de vetores

--- ordenar_vetores.c
+++ ordenar_vetores.c
@@ -1,11 +1,12 @@
 #include <limits.h>
 #include <stdio.h>
 #include "libprg/libprg.h"
+#include <sys/time.h>
 #include <time.h>
 
 void preencher_vetor(int *vetor, int tam)
 {
-    srand(1);
+    srand(time(0));
     for(int i = 0; i < tam; i++)
         vetor[i] = rand() % (100 + 1);
 }
@@ -19,16 +20,35 @@ void print_vetor(int *vetor, int tam)
 
 int main()
 {
+    struct timeval inicio, fim;
+
+    clock_t start, end;
+    double cpu_time_used;
+
     printf("Informe o tamanho do vetor: ");
     int tamanho;
     scanf("%d", &tamanho);
 
     int vetor[tamanho];
     preencher_vetor(vetor, tamanho);
-    print_vetor(vetor, tamanho);
+
+    gettimeofday(&inicio, 0);
+    start = clock();
 
     selection_sort(vetor, tamanho);
     print_vetor(vetor, tamanho);
+
+    gettimeofday(&fim, 0);
+    end = clock();
+
+    long seg = fim.tv_sec - inicio.tv_sec;
+    long mseg = fim.tv_usec - inicio.tv_usec;
+    double tempo_total = seg + mseg * 1e-6;
+
+    printf("Tempo gasto: %f segundos.\n", tempo_total);
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Tempo gasto de CPU: %f segundos.\n", cpu_time_used);
 
     return 0;
 }
